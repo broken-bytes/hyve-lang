f065fb6fbd755beede9c6c8ab8b4c43aa202ffa1
Marcel Kulina <marcel.kulina@outlook.de>
2024-07-25 00:25:41 +0200

    Change docs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Redirect } from '@docusaurus/router';
 
 function Home() {
-  return <Redirect to="/Hyve/docs/category/getting-started" />;
+  return <Redirect to="/docs/category/getting-started" />;
 }
 
 export default Home;
