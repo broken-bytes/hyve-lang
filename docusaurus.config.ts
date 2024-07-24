2157d3e129a008950306ae112d2e42472e370ee7
Marcel Kulina <marcel.kulina@outlook.de>
2024-07-24 21:43:54 +0200

    Add social card

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -56,7 +56,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/hyve-social-card.png',
     navbar: {
       title: 'Hyve Programming Language',
       logo: {
